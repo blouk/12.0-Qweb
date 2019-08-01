85d0c1e8ea7460dc7b5460baa75a27719c69e026
Blouk <nicolas@blouk.com>
2019-08-01 10:35:26 +0200

    [ADD] Manifest & structure

---
+++ website_qweb/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Website Qweb',
+    'category': 'Website',
+    'sequence': 7,
+    'summary': 'Some example of Qweb (website point of view)',
+    'version': '1.0',
+    'description': "",
+    'depends': [
+        'website',
+    ],
+    'installable': True,
+    'data': [
+
+    ],
+    'application': True,
+}
