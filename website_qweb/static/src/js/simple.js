7ef9cf1b3adf89b5fa0b19b081a6d06d98230e0c
Blouk <nicolas@blouk.com>
2019-08-01 17:35:49 +0200

    [ADD] change some things

--- website_qweb/static/src/js/simple.js
+++ website_qweb/static/src/js/simple.js
@@ -3,7 +3,6 @@ odoo.define('website_qweb.simpleJS', function(require) {
 
     require('web.dom_ready');
     // add your javascript here.
-    console.log('DOM.READY');
-
+    console.log("DOM.READY, it's a cool feature");
 
 });
