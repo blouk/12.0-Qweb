5aee4e96929cc13d2322e72ff09cb1684f84d24d
Blouk <nicolas@blouk.com>
2019-09-10 10:36:23 +0200

    [ADD] website theme example

---
+++ website_theme/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Website Theme',
+    'category': 'Website',
+    'sequence': 7,
+    'summary': 'A simple Theme example ',
+    'version': '1.0',
+    'description': "",
+    'depends': [
+        'website','theme_common,
+    ],
+    'installable': True,
+    'data': [
+
+    #website menu
+
+    #assets
+
+    #layout
+
+    #pages
+    ],
+    'application': True,
+}
