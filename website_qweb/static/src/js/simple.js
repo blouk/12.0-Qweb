f7e92c94596daea7742449e5d6ba1cca05b5e269
Blouk <nicolas@blouk.com>
2019-12-12 08:19:26 +0100

    [REM] from master

--- website_qweb/static/src/js/simple.js
+++
@@ -1,8 +0,0 @@
-odoo.define('website_qweb.simpleJS', function(require) {
-    'use strict';
-
-    require('web.dom_ready');
-    // add your javascript here.
-    console.log("DOM.READY, it's a cool feature");
-
-});
