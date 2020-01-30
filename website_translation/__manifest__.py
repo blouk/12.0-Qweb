d88860a60cfe8817bd9f6a55a99158cd0db16e0a
Blouk <nicolas@blouk.com>
2020-01-30 16:30:16 +0100

    [ADD] Translation

---
+++ website_translation/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Translation Website',
+    'category': 'Hidden',
+    'sequence': 7,
+    'summary': 'Translation Website Example ',
+    'version': '0.2',
+    'description': '',
+    'depends': ['website' ],
+    'data': [
+
+        #data
+        'data/menu.xml',
+        # pages
+        'views/pages/simple_page.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
